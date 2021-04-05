53b0939021b61a3290be66611f0d3760760d6823
rafel rojas <leonardorhojas@gmail.com>
2021-04-04 22:04:45 -0500

    feat: temporal changes

--- drawingTool.py
+++ drawingTool.py
@@ -1,51 +1,120 @@
+import abc
 import argparse
 import os.path
 from os import path
 import numpy as np
 
 
-class DrawingCanvas:
+class Shape:
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, canvas, points):
+        p1, p2 = points
+        self.x1, self.y1 = p1
+        self.x2, self.y2 = p2
+        self.canvas = canvas
+
+    @staticmethod
+    def range_correction(a: int, b: int):
+        if a < b:
+            line = (a - 1, b)
+        else:
+            line = (b - 1, a)
+        return line
+
+    @abc.abstractmethod
+    def draw(self):
+        pass
+
+
+class Line(Shape):
+
+    def draw(self):
+        if self.x1 == self.x2:
+            x = self.x1 - 1
+            line = self.range_correction(self.y1, self.y2)
+            for i in range(*line):
+                self.canvas[i][x] = 'x'
+        # draw horizontal line
+        elif self.y2 == self.y2:
+            y = self.y1 - 1
+            line = self.range_correction(x1, x2)
+            for i in range(*line):
+                self.canvas[y][i] = 'x'
+        else:
+            print('Not valid coordinates to plot a horizontal/vertical line')
+
+
+class Rectangle(Shape):
+
+    def __init__(self):
+        super().__init__(self)
+        self.p1_p = (self.x2, self.y1)
+        self.p2_p = (self.x1, self.y2)
+
+    def draw(self):
+        # TODO: include p1_p and p2_p in constructor
+        #self.p1_p = (self.x2, self.y1)
+        #self.p2_p = (self.x1, self.y2)
+        # draw_horizontal_lines
+        line_h1 = Line((self.p1, self.p1_p))
+        line_h1.draw()
+        #self.draw_line((self.p1, self.p1_p))
+        line_h2 = Line((self.p2_p, self.p2))
+        line_h2.draw()
+        #self.draw_line((self.p2_p, self.p2))
+
+        # draw_vertical_lines
+        line_v1 = Line((self.p1, self.p2_p))
+        line_v1.draw()
+        #self.draw_line((self.p1, self.p2_p))
+        line_v2 = Line((self.p1_p, self.p2))
+        line_v2.draw()
+        #self.draw_line((self.p1_p, self.p2))
+
+
+class Drawer:
 
     def __init__(self, canvas, instructions):
         self.canvas_det = canvas
         self.canvas = [[' ' for x in range(int(self.canvas_det[1]))] for y in range(int(self.canvas_det[2]))]
         self.instructions = instructions
 
-    def draw_line(self, x1, y1, x2, y2, show_canvas=True):
+    def draw_line(self, points: tuple):
         # TODO: Check for just horizontal and vertical lines
         # draw vertical line
+        p1, p2 = points
+        x1, y1 = p1
+        x2, y2 = p2
         if x1 == x2:
-            x = int(x1) - 1
-            for i in range(int(y1) - 1, int(y2)):
+            x = x1 - 1
+            line = self.range_correction(y1, y2)
+            for i in range(*line):
                 self.canvas[i][x] = 'x'
         # draw horizontal line
         elif y2 == y2:
-
-            y = int(y1) - 1
-            for i in range(int(x1) - 1, int(x2)):
+            y = y1 - 1
+            line = self.range_correction(x1, x2)
+            for i in range(*line):
                 self.canvas[y][i] = 'x'
         else:
-            print('Not valid coordinates to plot a line h/v')
+            print('Not valid coordinates to plot a horizontal/vertical line')
 
-        if show_canvas:
-            self.draw_canvas()
-
-    def draw_rectangle(self, x1, y1, x2, y2):
+    def draw_rectangle(self, points: tuple):
         # TODO: Validate range of canvas
         # calc implicit points
-        x1_p = x2
-        y1_p = y1
-        x2_p = x1
-        y2_p = y2
+        p1, p2 = points
+        x1, y1 = p1
+        x2, y2 = p2
+        p1_p = (x2, y1)
+        p2_p = (x1, y2)
         # draw_horizontal_lines
-        self.draw_line(x1, y1, x1_p, y1_p, False)
-        self.draw_line(x2_p, y2_p, x2, y2, False)
+        self.draw_line((p1, p1_p))
+        self.draw_line((p2_p, p2))
 
         # draw_vertical_lines
-        self.draw_line(x1, y1, x2_p, y2_p, False)
-        self.draw_line(x1_p, y1_p, x2, y2, False)
-
-        self.draw_canvas()
+        self.draw_line((p1, p2_p))
+        self.draw_line((p1_p, p2))
 
     def bucket_fill(self, start_coords, fill_value):
         """
@@ -92,13 +161,48 @@ class DrawingCanvas:
     def draw(self):
         # for step in list_steps:
         for instruction in self.instructions:
+            # Draw Line
             if instruction[0] == 'L':
-                # TODO: pass ordered the line coordinates
-                self.draw_line(int(instruction[1]), int(instruction[2]), int(instruction[3]), int(instruction[4]), True)
-            if instruction[0] == 'R':
-                self.draw_rectangle(int(instruction[1]), int(instruction[2]), int(instruction[3]), int(instruction[4]))
-            if instruction[0] == 'B':
-                self.bucket_fill((int(instruction[1]), int(instruction[2])), instruction[3])
+                if len(instruction) == 5:
+                    try:
+                        p1 = (int(instruction[1]), int(instruction[2]))
+                        p2 = (int(instruction[3]), int(instruction[4]))
+                        self.draw_line((p1, p2))
+                        self.draw_canvas()
+                    except ValueError:
+                        print('Invalid line arguments')
+                else:
+                    print('invalid number of arguments for Draw line')
+                    return
+            # Draw Rectangle
+            elif instruction[0] == 'R':
+                if len(instruction) == 5:
+                    try:
+                        p1 = (int(instruction[1]), int(instruction[2]))
+                        p2 = (int(instruction[3]), int(instruction[4]))
+                        self.draw_rectangle((p1, p2))
+                        self.draw_canvas()
+                    except ValueError:
+                        print('Invalid line arguments')
+                else:
+                    print('invalid number of arguments for Draw Rectangle')
+                    return
+            # Bucket fill
+            elif instruction[0] == 'B':
+                if len(instruction) == 4:
+                    try:
+                        point = (int(instruction[1]), int(instruction[2]))
+                        color = instruction[3]
+                        self.bucket_fill(point, color)
+                    except ValueError:
+                        print('Invalid line arguments')
+                else:
+                    print('invalid number of arguments for Bucket Fill')
+                    return
+            # No valid instruction
+            else:
+                print(f'invalid instruction {instruction[0]} in input file')
+                return
 
     def draw_canvas(self):
         width = len(self.canvas[0])
@@ -117,6 +221,14 @@ class DrawingCanvas:
                 ofile.write('\n')
             ofile.write(h_line)
             ofile.write('\n')
+
+    @staticmethod
+    def range_correction(a: int, b: int):
+        if a < b:
+            line = (a - 1, b)
+        else:
+            line = (b - 1, a)
+        return line
 
 
 def read_input_file(file) -> list:
@@ -141,10 +253,13 @@ def delete_previous_output():
 
 def main(input_file):
     canvas, instructions = read_input_file(input_file)
-    # TODO:  include checking valid instructions per line
+    if canvas[0] != 'C' or len(canvas) != 3:
+        print('ERROR: Not a valid input file, First command should be a Canvas + size i.e C [X], [Y]')
+        return "Not Valid canvas"
     delete_previous_output()
     # TODO: define canvas matrix as ndarray
-    dw = DrawingCanvas(canvas, instructions)
+    #cv = Canvas(canvas)
+    dw = Drawer(canvas, instructions)
     dw.draw_canvas()
     dw.draw()
 
@@ -156,4 +271,3 @@ if __name__ == '__main__':
     parser.add_argument('input_file', type=str, default='input.txt', help='provide the location of the input file')
     args = parser.parse_args()
     main(args.input_file)
-
--- test/test_drawing_tool.py
+++ test/test_drawing_tool.py
@@ -1,6 +1,4 @@
 import pytest
-import os.path
-from os import path
 import drawingTool
 # import mock
 
@@ -10,8 +8,7 @@ def read_file(file):
     return str
 
 
-def test_result():
-    # from drawingTool import main
+def test_main_result():
     # Given
     input_file = 'test/files/input.txt'
     output_generated = "output.txt"
